c76b65feedb3a53adf13d6786f9c128479eabf94
lzc <1771991075@qq.com>
2021-12-20 23:13:03 +0800

    修复下滑按钮

--- src/home/index.php
+++ src/home/index.php
@@ -34,7 +34,7 @@
   <div class="container-fulid">
     <a class="an">
       <span>
-        <svg onclick="window.scrollTo(0,775);" xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="white" class="bi bi-chevron-compact-down animate__animated animate__bounce animate__slow animate__infinite" viewBox="0 0 16 16">
+        <svg onclick="$('html,body').animate({ scrollTop: 775 }, 50);" xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="white" class="bi bi-chevron-compact-down animate__animated animate__bounce animate__slow animate__infinite" viewBox="0 0 16 16">
           <path fill-rule="evenodd" d="M1.553 6.776a.5.5 0 0 1 .67-.223L8 9.44l5.776-2.888a.5.5 0 1 1 .448.894l-6 3a.5.5 0 0 1-.448 0l-6-3a.5.5 0 0 1-.223-.67z"/>
         </svg>
       </span>
